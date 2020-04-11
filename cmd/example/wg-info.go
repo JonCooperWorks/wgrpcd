3175f27782ab1f303d2d372ab696f9a1b68f53dc
JonCooperWorks <jonathan.cooper.2010@gmail.com>
2020-04-11 10:01:45 -0500

    Update wg-info.go

--- cmd/example/wg-info.go
+++ cmd/example/wg-info.go
@@ -7,21 +7,6 @@ import (
 	wireguard "github.com/joncooperworks/wireguardrpc"
 )
 
-type PeerConfigFile struct {
-	PrivateKey string
-	PublicKey  string
-	AllowedIPs []net.IPNet
-}
-
-func MustParseCIDR(address string) *net.IPNet {
-	_, net, err := net.ParseCIDR(address)
-	if err != nil {
-		panic(err)
-	}
-
-	return net
-}
-
 func main() {
 
 	devices, err := wireguard.Devices()
