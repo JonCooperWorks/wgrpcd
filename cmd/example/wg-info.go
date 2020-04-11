a08785e2df16dee2ab71527f74e8a69d95772a84
JonCooperWorks <jonathan.cooper.2010@gmail.com>
2020-04-11 10:01:14 -0500

    Update wg-info.go

--- cmd/example/wg-info.go
+++ cmd/example/wg-info.go
@@ -5,7 +5,6 @@ import (
 	"net"
 
 	wireguard "github.com/joncooperworks/wireguardrpc"
-	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
 type PeerConfigFile struct {
@@ -36,59 +35,10 @@ func main() {
 
 	log.Println("Found devices: ", devices)
 	device := &wireguard.Wireguard{DeviceName: "wg0"}
-
 	peers, err := device.Peers()
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	for _, peer := range peers {
-		log.Println(peer)
-	}
 
-	allowedIPs := []net.IPNet{
-		*MustParseCIDR("0.0.0.0/0"),
-		*MustParseCIDR("::/0"),
-	}
-
-	key, err := wgtypes.GeneratePrivateKey()
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	peerConfig, err := device.AddNewPeer(allowedIPs, key.PublicKey())
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	peers, err = device.Peers()
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	log.Println("Before:")
-	for _, peer := range peers {
-		log.Println(peer)
-	}
-
-	key, err = wgtypes.GeneratePrivateKey()
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-	device.RekeyClient(allowedIPs, peerConfig.PublicKey, key.PublicKey())
-
-	peers, err = device.Peers()
-	if err != nil {
-		log.Fatalln(err.Error())
-	}
-
-	log.Println("After:")
-	for _, peer := range peers {
-		log.Println(peer)
-	}
-	peerConfigFile := PeerConfigFile{
-		PublicKey:  peerConfig.PublicKey.String(),
-		PrivateKey: key.String(),
-		AllowedIPs: allowedIPs,
-	}
-
-	log.Println(peerConfigFile)
+	log.Println(peers)
 }
