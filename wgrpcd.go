64c70bcb5d0ce89a506368f67f8a44e56331668b
Jonathan Cooper <cooper@cooperthecoder.com>
2020-10-16 15:05:06 -0500

    Added package comment

---
+++ wgrpcd.go
@@ -0,0 +1,3 @@
+// Package wgrpcd contains an opinionated Wireguard VPN controller that accepts connections over gRPC and mTLS with TLSv1.3.
+// wgrpcd can be used as a library but is meant to be used with its included wgrpcd CLI.
+package wgrpcd
